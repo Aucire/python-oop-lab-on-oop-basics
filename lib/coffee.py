818ec0580bdc33c6825912766b95638a9e3c5a8a
Aucire <jamesandaro0@gmail.com>
2026-09-15 09:39:15 +0300

    Added the book and class models

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self,size,price):
+        if size!="Small" or size!="Medium" or size!="Large":
+            raise ValueError("size must be Small, Medium, or Large")
+        
+        self.size=size
+        self.price=price
+
+    def tip(self):
+        print(f"This coffee is great, here’s a tip!")
+        self.price+=1
+        return self.price

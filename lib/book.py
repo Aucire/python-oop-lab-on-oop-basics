2ae5e4757829cab2930d1cb524f265a79d14a5ec
James Andaro Osire <112567049+Aucire@users.noreply.github.com>
2026-09-15 10:05:26 +0300

    Add Book class with initialization and page turning
    Implemented constructor and page turning method for Book class.

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self,title,page_count):
+        self.title=title
+
+        if page_count is not int:
+            raise ValueError("page_count must be an integer")
+        self.page_count=page_count
+
+    def turn_page(self):
+        print(f"Flipping the page...wow, you read fast!")  
